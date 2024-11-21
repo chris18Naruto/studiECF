e45d2bb15fe2b9f0aba74595775da00a9204d47a
bryan <chris_camp-@hotmail.fr>
2024-11-21 23:15:27 +0100

    init

---
+++ space_work/exemple.js
@@ -0,0 +1,22 @@
+// let currentSlide = 0;
+// const slides = document.querySelectorAll('.image');
+
+// function changeSlide(direction) {
+//     slides[currentSlide].classList.remove('active');
+//     currentSlide = (currentSlide + direction + slides.length) % slides.length;
+//     slides[currentSlide].classList.add('active');
+//     updateCarrousel();
+// }
+
+// function updateCarrousel() {
+//     const offset = -currentSlide * 100;
+//     document.querySelector('.carrousel-images').style.transform = `translateX(${offset}%)`;
+// }
+
+// function autoChangeSlide() {
+//     changeSlide(1);
+// }
+
+// setInterval(autoChangeSlide, 3000); // Changer d'image toutes les 3 secondes
+
+
